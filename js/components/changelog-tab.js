75370c7bf60b5cf67f7f7a2731b7c204ccd58282
JoseGamaTheMapper <94137841+JoseGamaAUTTP@users.noreply.github.com>
2022-05-22 15:35:56 -0300

    Update changelog-tab.js

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,8 +1,18 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
+    <template v-slot:title>v1.0.1</template>
+    <template v-slot:text>Made the new sticker updated with new words
+    </template>
+    </guide-item>
+</div>`
+})
+
+Vue.component("changelog-tab", {
+    template: `<div class="changelog-tab">
+    <guide-item>
     <template v-slot:title>v1.0.0</template>
-    <template v-slot:text>When modding, edit this when you make new releases!
+    <template v-slot:text>Just the release of the game
     </template>
     </guide-item>
 </div>`
